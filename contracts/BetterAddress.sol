52f4f5b81debc5b660d42b49696dbecbbb492c13
peter borah <peterborah@gmail.com>
2016-04-06 23:29:04 -0500

    Properly handle memory.

--- contracts/BetterAddress.sol
+++ contracts/BetterAddress.sol
@@ -10,10 +10,13 @@ contract BetterAddress {
   function() {
     var (destination, outsize) = resolver.lookup(msg.sig);
     assembly {
-      calldatacopy(mload(0x40), 0, calldatasize)
-      mstore(add(mload(0x40), calldatasize), 0x40)
-      let r := call(1000000, destination, 0, mload(0x40), calldatasize, mload(0x40), outsize)
-      return(mload(0x40), outsize)
+      let inloc := mload(0x40)
+      calldatacopy(inloc, 0, calldatasize)
+      let outloc := add(inloc, calldatasize)
+      mstore(0x40, outloc)
+      let r := callcode(1000000, destination, 0, inloc, calldatasize, outloc, outsize)
+      mstore(0x40, add(outloc, outsize))
+      return(outloc, outsize)
     }
   }
 }
