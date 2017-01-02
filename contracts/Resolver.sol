4290ff9b65f4d1a2b1a12b76e27e7531d0ce71dc
peter borah <peterborah@gmail.com>
2017-01-02 16:21:13 -0600

    Pull out a method in resolver for getting length.

--- contracts/Resolver.sol
+++ contracts/Resolver.sol
@@ -49,21 +49,24 @@ contract Resolver {
       outsize = pointers[sig].outsize;
     }
 
-    // Get dynamic return size, if necessary
     if (length_pointers[sig].destination != 0) {
-      uint r;
-      address length_destination = length_pointers[sig].destination;
-      bytes4 length_sig = length_pointers[sig].sig;
-
-      assembly {
-        mstore(mload(0x40), length_sig)
-        calldatacopy(add(4, mload(0x40)), 4, sub(calldatasize, 4))
-        r := delegatecall(sub(gas, 700), length_destination, mload(0x40), calldatasize, mload(0x40), 32)
-        outsize := mul(mload(0x40), 32)
-      }
-
-      // Throw if the call failed
-      if (r != 1) { throw;}
+      outsize = getLength(sig, msg_data);
     }
+  }
+
+  function getLength(bytes4 sig, bytes msg_data) returns(uint outsize) {
+    uint r;
+    address length_destination = length_pointers[sig].destination;
+    bytes4 length_sig = length_pointers[sig].sig;
+
+    assembly {
+      mstore(mload(0x40), length_sig)
+      calldatacopy(add(4, mload(0x40)), 4, sub(calldatasize, 4))
+      r := delegatecall(sub(gas, 700), length_destination, mload(0x40), calldatasize, mload(0x40), 32)
+      outsize := mul(mload(0x40), 32)
+    }
+
+    // Throw if the call failed
+    if (r != 1) { throw;}
   }
 }
